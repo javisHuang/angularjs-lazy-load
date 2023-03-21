dff7cb40c694fde3c8bae1e3034a26dc9e69318b
javis <huang.javis@gmail.com>
2023-03-21 17:36:40 +0800

    修正下拉scroll無法載入資料問題，調整scroll判斷

--- angular-lazy-load.js
+++ angular-lazy-load.js
@@ -1,75 +1,74 @@
 (function(window, angular, undefined) {
-	'use strict';
+    'use strict';
 
-	angular.module('angularLazyLoad', [])
+    angular.module('angularLazyLoad', [])
+        .directive('usLazyLoadRepeatable', function($timeout){
+            var lazyLoad = {};
 
-		.directive('usLazyLoadRepeatable', ['$rootScope', '$window', '$timeout', function($rootScope, $window, $timeout){
-			var lazyLoad = {};
+            lazyLoad.itemCount = 15;
+            lazyLoad.factor = 2;
 
-			lazyLoad.itemCount = 15;
-			lazyLoad.factor = 2;
+            return {
+                scope: {
+                    lazyList: '=',
+                    lazyItemCount: '=',
+                    lazyCuttedlist: '='
+                },
+                link: function(scope, element, attr) {
+                    lazyLoad.element = element;
 
-			return {
-				scope: {
-					lazyList: '=',
-					lazyItemCount: '='
-				},
-				link: function(scope, element, attr) {
+                    lazyLoad.itemCount = (scope.lazyItemCount == null || scope.lazyItemCount == undefined) ? lazyLoad.itemCount : scope.lazyItemCount;
 
-					lazyLoad.itemCount = (scope.lazyItemCount == null || scope.lazyItemCount == undefined) ? lazyLoad.itemCount : scope.lazyItemCount;
+                    lazyLoad.handler = function() {
 
-					lazyLoad.handler = function() {
+                        if (scope.lazyList == null || scope.lazyList == undefined)
+                            return;
 
-						if (scope.lazyList == null || scope.lazyList == undefined)
-							return;
+                        var shouldExtend, windowBottom;
 
-						var elementBottom, remaining, shouldExtend, windowBottom;
+                        windowBottom = element[0].clientHeight + element[0].scrollTop+element[0].offsetTop;
+                        shouldExtend = windowBottom >= element[0].scrollHeight;
 
-						windowBottom = $window.innerHeight + $window.scrollY;
-						elementBottom = element[0].offsetTop + element[0].clientHeight;
-						remaining = elementBottom - windowBottom;
-						shouldExtend = remaining <= $window.innerHeight * 0;
+                        if (shouldExtend) {
+                            $timeout(function(){
+                                scope.lazyCuttedlist = scope.lazyList.slice(0, lazyLoad.itemCount * lazyLoad.factor);
 
-						if (shouldExtend) {
-							$timeout(function(){
-								$rootScope.cuttedList = scope.lazyList.slice(0, lazyLoad.itemCount * lazyLoad.factor);
+                                if (scope.lazyList.length <= lazyLoad.itemCount * lazyLoad.factor) {
 
-								if (scope.lazyList.length <= lazyLoad.itemCount * lazyLoad.factor) {
+                                    lazyLoad.element.off('scroll', lazyLoad.handler);
+                                }
 
-									angular.element($window).off('scroll', lazyLoad.handler);
-								}
+                                lazyLoad.factor += 1;
+                            });
+                        }
+                    };
 
-								lazyLoad.factor += 1;
-							});
-						}
-					};
+                    scope.$on('$destroy', function() {
 
-					scope.$on('$destroy', function() {
+                        scope.lazyList = undefined;
+                        return lazyLoad.element.off('scroll', lazyLoad.handler);
+                    });
 
-						scope.lazyList = undefined;
-						return angular.element($window).off('scroll', lazyLoad.handler);
-					});
-					
-					angular.element($window).bind('scroll', lazyLoad.handler);
+                    lazyLoad.element.bind('scroll', lazyLoad.handler);
 
-					if (scope.lazyList != null && scope.lazyList != undefined)
-						$rootScope.cuttedList = scope.lazyList.slice(0, lazyLoad.itemCount);
-				},
-				controller: function($scope) {
-					$scope.$watch('lazyList', function(newV, oldV){
-						$timeout(function(){
-							if (newV != null && newV != undefined) {
-								
-								$rootScope.cuttedList = undefined;
-								angular.element($window).off('scroll', lazyLoad.handler);
-								angular.element($window).bind('scroll', lazyLoad.handler);
-								$rootScope.cuttedList = newV.slice(0, lazyLoad.itemCount);
-								lazyLoad.factor = 2;
-							}
-						});
-					});
-				}
-			};
-		}]);
-	
+                    if (scope.lazyList != null && scope.lazyList != undefined)
+                        scope.lazyCuttedlist = scope.lazyList.slice(0, lazyLoad.itemCount);
+                },
+                controller: function($scope) {
+                    $scope.$watch('lazyList', function(newV, oldV){
+                        $timeout(function(){
+                            if (newV != null && newV != undefined) {
+
+                                $scope.lazyCuttedlist = undefined;
+                                lazyLoad.element.off('scroll', lazyLoad.handler);
+                                lazyLoad.element.bind('scroll', lazyLoad.handler);
+                                $scope.lazyCuttedlist = newV.slice(0, lazyLoad.itemCount);
+                                lazyLoad.factor = 2;
+                            }
+                        });
+                    });
+                }
+            };
+        });
+
 })(window, window.angular);
